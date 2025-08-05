6bf8a219c9c658201a3a4c0efdab3c8a8a1cde53
Felipe <felipequada@gmail.com>
2025-08-05 16:32:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import githubIcon from "./assets/github-mark-white.svg";
+import linkedinIcon from "./assets/linkedin-app-white-icon.webp";
+import userIcon from "./assets/user.svg";
+import projectsIcon from "./assets/rocket.svg";
+import linkButton from "./components/link-button.vue";
+</script>
+
+<template>
+  <div class="mainDiv">
+    <img src="https://avatars.githubusercontent.com/u/96788382" class="photo" />
+
+    <div class="textDiv">
+      <h1>Felipe Quaresma</h1>
+      <h2>Full-Stack Developer</h2>
+    </div>
+
+    <link-button
+      :img-src="githubIcon"
+      link="https://github.com/FeQuaresma"
+      name="Github"
+    />
+    <link-button
+      :img-src="linkedinIcon"
+      link="https://www.linkedin.com/in/fequaresma/"
+      name="Linkedin"
+    />
+    <link-button :img-src="userIcon" link="/about-me" name="About me" />
+    <link-button :img-src="projectsIcon" link="/projects" name="Projects" />
+  </div>
+</template>
+
+<style scoped>
+.mainDiv {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  align-items: center;
+}
+
+.textDiv {
+}
+
+.photo {
+  border-radius: 100%;
+  height: 13em;
+  width: 13em;
+}
+</style>
---
+++ src/components/link-button.vue
@@ -0,0 +1,27 @@
+<template>
+  <a target="_blank" :href="link" class="link"
+    ><button>
+      <img v-bind:src="imgSrc" class="icon" />{{ name }}
+    </button></a
+  >
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    link: string;
+    imgSrc: string;
+    name: string;
+}>();
+</script>
+
+<style>
+.icon {
+  height: 0.8em;
+  margin-right: 0.5em;
+}
+
+.link:hover {
+  border-width: 0;
+  filter: drop-shadow(0 0 1em #ffffff);
+}
+</style>
